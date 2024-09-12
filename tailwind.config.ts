c25c4c53a63628c8620ffdf9359e4e5f9eacc8bd
Kgothatso Ntsane <nvisionfactory@gmail.com>
2024-09-12 12:21:09 +0000

    Update dependencies and color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
-import type { Config } from "tailwindcss";
+/** import type { Config } from "tailwindcss"; */
+import { withUt } from 'uploadthing/tw'
 
-const config: Config = {
+module.exports = withUt({
     darkMode: ["class"],
     content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -81,5 +82,4 @@ const config: Config = {
   	}
   },
   plugins: [require("tailwindcss-animate")],
-};
-export default config;
+})
