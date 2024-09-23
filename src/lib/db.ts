11edcce01de80d5a56c1e5f5a80097c86b436de0
tristangms <nvisionfactory@gmail.com>
2024-09-23 17:32:37 +0200

    Refactor Prisma database client and configuration

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 
+
 declare global {
-  var prisma: PrismaClient | undefined
+
+    // eslint-disable-next-line no-var
+    var prisma: PrismaClient | undefined
 }
 
 export const db = globalThis.prisma || new PrismaClient()
