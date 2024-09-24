c1a179c2c39b8b34c72b295094ae97b461e613ee
tristangms <nvisionfactory@gmail.com>
2024-09-25 01:58:37 +0200

    Refactor imports to use server-side version of @clerk/nextjs

--- src/app/(main)/subaccount/[subaccountId]/layout.tsx
+++ src/app/(main)/subaccount/[subaccountId]/layout.tsx
@@ -2,9 +2,9 @@ import InfoBar from '@/components/global/infobar'
 import Sidebar from '@/components/sidebar'
 import Unauthorized from '@/components/unauthorized'
 import {
-  getAuthUserDetails,
-  getNotificationAndUser,
-  verifyAndAcceptInvitation,
+    getAuthUserDetails,
+    getNotificationAndUser,
+    verifyAndAcceptInvitation,
 } from '@/lib/queries'
 import { currentUser } from '@clerk/nextjs/server'
 import { Role } from '@prisma/client'
@@ -12,64 +12,64 @@ import { redirect } from 'next/navigation'
 import React from 'react'
 
 type Props = {
-  children: React.ReactNode
-  params: { subaccountId: string }
+    children: React.ReactNode
+    params: { subaccountId: string }
 }
 
 const SubaccountLayout = async ({ children, params }: Props) => {
-  const agencyId = await verifyAndAcceptInvitation()
-  if (!agencyId) return <Unauthorized />
-  const user = await currentUser()
-  if (!user) {
-    return redirect('/')
-  }
-
-  let notifications: Array<{ subAccountId: string; [key: string]: any }> = []
-
-  if (!user.privateMetadata.role) {
-    return <Unauthorized />
-  } else {
-    const allPermissions = await getAuthUserDetails()
-    const hasPermission = allPermissions?.Permissions.find(
-      (permissions) =>
-        permissions.access && permissions.subAccountId === params.subaccountId
-    )
-    if (!hasPermission) {
-      return <Unauthorized />
+    const agencyId = await verifyAndAcceptInvitation()
+    if (!agencyId) return <Unauthorized />
+    const user = await currentUser()
+    if (!user) {
+        return redirect('/')
     }
 
-    const allNotifications = await getNotificationAndUser(agencyId)
+    let notifications: any = []
 
-    if (
-      user.privateMetadata.role === 'AGENCY_ADMIN' ||
-      user.privateMetadata.role === 'AGENCY_OWNER'
-    ) {
-      notifications = allNotifications
+    if (!user.privateMetadata.role) {
+        return <Unauthorized />
     } else {
-      const filteredNoti = allNotifications?.filter(
-        (item) => item.subAccountId === params.subaccountId
-      )
-      if (filteredNoti) notifications = filteredNoti
+        const allPermissions = await getAuthUserDetails()
+        const hasPermission = allPermissions?.Permissions.find(
+            (permissions) =>
+                permissions.access && permissions.subAccountId === params.subaccountId
+        )
+        if (!hasPermission) {
+            return <Unauthorized />
+        }
+
+        const allNotifications = await getNotificationAndUser(agencyId)
+
+        if (
+            user.privateMetadata.role === 'AGENCY_ADMIN' ||
+            user.privateMetadata.role === 'AGENCY_OWNER'
+        ) {
+            notifications = allNotifications
+        } else {
+            const filteredNoti = allNotifications?.filter(
+                (item) => item.subAccountId === params.subaccountId
+            )
+            if (filteredNoti) notifications = filteredNoti
+        }
     }
-  }
 
-  return (
-    <div className="h-screen overflow-hidden">
-      <Sidebar
-        id={params.subaccountId}
-        type="subaccount"
-      />
+    return (
+        <div className="h-screen overflow-hidden">
+            <Sidebar
+                id={params.subaccountId}
+                type="subaccount"
+            />
 
-      <div className="md:pl-[300px]">
-        <InfoBar
-          notifications={notifications}
-          role={user.privateMetadata.role as Role}
-          subAccountId={params.subaccountId as string}
-        />
-        <div className="relative">{children}</div>
-      </div>
-    </div>
-  )
-} 
+            <div className="md:pl-[300px]">
+                <InfoBar
+                    notifications={notifications}
+                    role={user.privateMetadata.role as Role}
+                    subAccountId={params.subaccountId as string}
+                />
+                <div className="relative">{children}</div>
+            </div>
+        </div>
+    )
+}
 
 export default SubaccountLayout
