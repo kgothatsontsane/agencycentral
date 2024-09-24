546a69cfcab149e5ebf6d0f8420da06a0facb0c0
tristangms <nvisionfactory@gmail.com>
2024-09-24 20:10:09 +0200

    Refactor middleware.ts to update authentication logic and fix NextRequest import

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse, NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
-const isPublicRoute = createRouteMatcher(['/site', '/api/uploadthing', '/sign-in', '/sign-up']);
+const isPublicRoute = createRouteMatcher(['/site', '/sign-in', '/sign-up', '/api/uploadthing']);
 
 const isPublicApiRoute = createRouteMatcher([
     "/api/uploadthing"
@@ -9,71 +9,54 @@ const isPublicApiRoute = createRouteMatcher([
 
 
 export default clerkMiddleware((auth, req) => {
-    req = new NextRequest(new URL('https://agencycentral.vercel.app'), {
-        headers: {
-            host: 'agencycentral.vercel.app',
-        },
-    });
     const url = new URL(req.url);
     const searchParams = url.searchParams.toString();
-    const hostname = req.headers.get('host');
+    const hostname = req.headers;
     const isApiRequest = url.pathname.startsWith("/api")
 
 
-    let debugInfo = `URL: ${url}\nSearch Params: ${searchParams}\nHostname: ${hostname}\n`;
+    // If the request is for a protected route and the user is not logged in
 
     if (!isPublicRoute(req)) {
         auth().protect();
     }
 
-    const pathWithSearchParams = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ''}`;
-    debugInfo += `Path with Search Params: ${pathWithSearchParams}\n`;
+    const pathWithSearchParams = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ''}`
 
     // If subdomain exists
-    const customSubDomain = hostname?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`).filter(Boolean)[0];
-    debugInfo += `Custom Subdomain: ${customSubDomain}\n`;
+    const customSubDomain = hostname
+        .get('host')
+        ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+        .filter(Boolean)[0]
 
     if (customSubDomain) {
-        debugInfo += `Rewriting to: /${customSubDomain}${pathWithSearchParams}\n`;
-        return NextResponse.rewrite(new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)).headers.set('X-Debug-Info', debugInfo);
+        return NextResponse.rewrite(
+            new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
+        )
     }
 
     if (url.pathname === '/sign-in' || url.pathname === '/sign-up') {
-        debugInfo += `Redirecting to: /agency/sign-in\n`;
-        return NextResponse.redirect(new URL(`/agency/sign-in`, req.url)).headers.set('X-Debug-Info', debugInfo);
+        return NextResponse.redirect(new URL(`/agency/sign-in`, req.url))
     }
 
-    if (url.pathname === '/' || (url.pathname === '/site' && hostname === process.env.NEXT_PUBLIC_DOMAIN)) {
-        debugInfo += `Rewriting to: /site\n`;
-        return NextResponse.rewrite(new URL('/site', req.url)).headers.set('X-Debug-Info', debugInfo);
+    if (
+        url.pathname === '/' ||
+        (url.pathname === '/site' && url.host === process.env.NEXT_PUBLIC_DOMAIN)
+    ) {
+        return NextResponse.rewrite(new URL('/site', req.url))
+    }
+
+    if (
+        url.pathname.startsWith('/agency') ||
+        url.pathname.startsWith('/subaccount')
+    ) {
+        return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url))
     }
     
-/*    if (url.pathname === '/' && hostname === process.env.NEXT_PUBLIC_DOMAIN) {
-        debugInfo += `Rewriting to: /site\n`;
-        return NextResponse.rewrite(new URL('/site', req.url)).headers.set('X-Debug-Info', debugInfo);
-    }
-*/   
-    if (url.pathname.startsWith('/agency') || url.pathname.startsWith('/subaccount')) {
-        debugInfo += `Rewriting to: ${pathWithSearchParams}\n`;
-        return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url)).headers.set('X-Debug-Info', debugInfo);
-    }
-
     // If the request is for a protected API and the user is not logged in
     if (isApiRequest && !isPublicApiRoute(req)) {
         return NextResponse.redirect(new URL("/sign-in", req.url))
     }
-    
-    // Handle /test route
-    if (url.pathname === '/test') {
-        debugInfo += `Handling /test route\n`;
-        const response = new NextResponse('Successful message. Redirecting to /site...', { status: 200 });
-        response.headers.set('Refresh', '3;url=/site'); // Redirect after 3 seconds
-        response.headers.set('X-Debug-Info', debugInfo);
-        return response;
-    }
-
-    debugInfo += `Proceeding to NextResponse.next()\n`;
-    return NextResponse.next().headers.set('X-Debug-Info', debugInfo);
 });
 
 
