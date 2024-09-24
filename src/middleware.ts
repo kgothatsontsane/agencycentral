22959716e61c210bc2e72ea2b7d5983363ad2dd6
tristangms <nvisionfactory@gmail.com>
2024-09-24 17:30:22 +0200

    Refactor middleware to handle subdomains and URL parameters

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-const isPublicRoute = createRouteMatcher(['/site', '/api/uploadthing', '/sign-in', '/sign-up']);
+const isPublicRoute = createRouteMatcher(['/','/site', '/api/uploadthing', '/sign-in', '/sign-up']);
 
 export default clerkMiddleware((auth, req) => {
-    const url = req.nextUrl;
-    const searchParams = url.searchParams.toString();
+    const url = new URL(req.url);
+    const searchParams = url;
     const hostname = req.headers.get('host');
 
     if (!isPublicRoute(req)) {
@@ -15,9 +15,9 @@ export default clerkMiddleware((auth, req) => {
     const pathWithSearchParams = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ''}`;
 
     // If subdomain exists
-    const customSubDomain = hostname?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`).filter(Boolean)[0];
+    const customSubDomain = hostname?.split(`.${process.env.NEXT_PUBLIC_DOMAIN}`)[0];
 
-    if (customSubDomain) {
+    if (customSubDomain && customSubDomain !== 'www') {
         return NextResponse.rewrite(new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url));
     }
 
@@ -25,7 +25,7 @@ export default clerkMiddleware((auth, req) => {
         return NextResponse.redirect(new URL(`/agency/sign-in`, req.url));
     }
 
-    if (url.pathname === '/' || (url.pathname === '/site' && url.host === process.env.NEXT_PUBLIC_DOMAIN)) {
+    if (url.pathname === '/' || (url.pathname === '/site' && hostname === process.env.NEXT_PUBLIC_DOMAIN)) {
         return NextResponse.rewrite(new URL('/site', req.url));
     }
     
