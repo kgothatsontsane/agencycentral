97d5a0fd649e88029a63df5faac83167d7fa5e96
tristangms <nvisionfactory@gmail.com>
2024-09-25 09:26:26 +0200

    Refactor middleware.ts to update authentication logic and fix NextRequest import - protected all non public routes and added protection for API routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,22 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher(['/site', '/api/uploadthing', '/sign-in', '/sign-up', '/agency/sign-in', '/agency/sign-up']);
-
+const isPublicApiRoute = createRouteMatcher([
+    "/api/uploadthing",
+])
 export default clerkMiddleware(async (auth, req) => {
     const { userId } = auth();
     const url = req.nextUrl;
     const hostname = req.headers.get('host') || '';
     const searchParams = url.searchParams.toString();
     const pathWithSearchParams = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ''}`;
-    
+    const isApiRequest = url.pathname.startsWith("/api")
     
 
     
     // Before Auth Middleware
     if (!userId) {
+    
         // Redirect root ('/') to '/site' if user is not authenticated
         if (url.pathname === '/') {
             return NextResponse.rewrite(new URL('/site', req.url));
@@ -27,6 +30,12 @@ export default clerkMiddleware(async (auth, req) => {
         if (!isPublicRoute(req)) {
             return NextResponse.redirect(new URL(`/agency/sign-in`, req.url));
         }
+        
+        // If the request is for a protected API and the user is not logged in
+        if (isApiRequest && !isPublicApiRoute(req)) {
+            return NextResponse.redirect(new URL("/sign-in", req.url))
+        }
+        
         return NextResponse.next();
         
     }
